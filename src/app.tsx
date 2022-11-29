057ba2fee0a5c6bac21045987e40772096a449e6
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2022-11-30 00:56:29 +0300

    clean up

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class App extends React.Component {
+	public override render(): JSX.Element {
+		return (
+			<>
+				<div>Hello, World!</div>
+			</>
+		);
+	}
+}
